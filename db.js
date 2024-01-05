c49e56a0ce9864a0f64a4e852588f8c65a68f419
venu-webdev <venu24.work@gmail.com>
2024-01-05 17:57:39 +0530

    v1.3

--- db.js
+++ db.js
@@ -5,7 +5,10 @@ mongoose.connect("mongodb://localhost:27017/todo-app")
 const todoSchema = mongoose.Schema({
     title: String,
     description: String,
-    completed: Boolean
+    completed:{
+        type: Boolean,
+        default: false
+    }
 })
 
 const todo = mongoose.model('todos', todoSchema);
